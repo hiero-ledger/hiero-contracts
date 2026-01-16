4c8bf3f21fb1c0a41931251ac0d940fce385a59f
Mariusz Jasuwienas <jasuwienas@gmail.com>
2026-01-16 13:12:32 +0100

    chore: port supervising
    Signed-off-by: Mariusz Jasuwienas <jasuwienas@gmail.com>

--- .github/scripts/supervise-cn-port.sh
+++ .github/scripts/supervise-cn-port.sh
@@ -3,22 +3,19 @@
 # Forward MirrorNode port
 kubectl port-forward "$(kubectl get pods -A | grep 'mirror-1-rest-' | awk '{print $2}')" \
   -n "$(kubectl get namespaces | grep solo | grep -v 'solo-setup' | awk '{print $1}')" \
-  5551:5551 >/dev/null 2>&1
-kubectl port-forward network-node1-0 \
-  -n "$(kubectl get namespaces | grep solo | grep -v 'solo-setup' | awk '{print $1}')" \
-  50211:50211 \
-  >/dev/null 2>&1
+  5551:5551 >/dev/null 2>&1 &
+
 # NOTE:
 # I’m running solo with `force port enabled = true`.
 # In GitHub Actions, the port-forward stops abruptly, which shouldn’t happen.
 # I’m not sure why this occurs yet and need to investigate or ask someone about it.
 # For now, this script is a workaround to keep the port available.
 
-#while true; do
-#  (echo >/dev/tcp/127.0.0.1/50211) >/dev/null 2>&1 || \
-#    kubectl port-forward network-node1-0 \
-#      -n "$(kubectl get namespaces | grep solo | grep -v 'solo-setup' | awk '{print $1}')" \
-#      50211:50211 \
-#      >/dev/null 2>&1
-#  sleep 5
-#done
+while true; do
+  (nc -z localhost 50211) >/dev/null 2>&1 || \
+    kubectl port-forward network-node1-0 \
+      -n "$(kubectl get namespaces | grep solo | grep -v 'solo-setup' | awk '{print $1}')" \
+      50211:50211 \
+      >/dev/null 2>&1 &
+  sleep 5
+done
