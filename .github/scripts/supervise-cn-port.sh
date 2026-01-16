3d6a2d5668895211d851ee8a822836d1582009a6
Mariusz Jasuwienas <jasuwienas@gmail.com>
2026-01-16 10:48:01 +0100

    chore: port supervising
    Signed-off-by: Mariusz Jasuwienas <jasuwienas@gmail.com>

--- .github/scripts/supervise-cn-port.sh
+++ .github/scripts/supervise-cn-port.sh
@@ -5,12 +5,12 @@
 # In GitHub Actions, the port-forward stops abruptly, which shouldn’t happen.
 # I’m not sure why this occurs yet and need to investigate or ask someone about it.
 # For now, this script is a workaround to keep the port available.
-# Export port once
 
-#while true; do
-#  nc -z localhost 50211 >/dev/null 2>&1 || \
+while true; do
+  (echo >/dev/tcp/127.0.0.1/50211) >/dev/null 2>&1 || \
     kubectl port-forward network-node1-0 \
       -n "$(kubectl get namespaces | grep solo | grep -v 'solo-setup' | awk '{print $1}')" \
-      50211:50211
-#  sleep 1
-#done
+      50211:50211 \
+      >/dev/null 2>&1
+  sleep 5
+done
